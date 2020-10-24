f40f1e10cd2bdc998c9832759808f06eafc65da4
Fardeen Ahmed <47289822+Th3-N3rd@users.noreply.github.com>
2020-10-24 03:39:10 -0700

    Update Hello world.cpp

--- Hello world.cpp
+++ Hello world.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
+#include<iostream> //These are called preprocessors. One can also use <bits/stdc++.h? in place of <iostream>
 using namespace std;
-int main()
+int main() //This are is known for specifying gobal variable. Whenever global variable is to be spcified, it is to be here in C++.
 {
-	cout << "Hello world!!!";
-	return 0;
+	cout << "Hello world!!!"; //This statement will be printed.
+	return 0; //This returns the output with 0 or null output.
 }
